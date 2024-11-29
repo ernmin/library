67b727a085fba731b8c8a7ed4bea721ffb6edf44
ernmin <42770608+ernmin@users.noreply.github.com>
2024-11-30 00:01:30 +0800

    Refactor the Object Consctructor with Class

--- script.js
+++ script.js
@@ -30,12 +30,26 @@ function closeModal(modal) {
 
 const myLibrary = [];
 
-function Book(title, author, pages, read){
+/*function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
     this.info = function() {
+        let book_info = this.title + ' by ' + this.author + ', ' + this.pages + ', ' + this.read;
+        return book_info;
+    }
+}*/
+
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    info() {
         let book_info = this.title + ' by ' + this.author + ', ' + this.pages + ', ' + this.read;
         return book_info;
     }
